b56748916e979a842d0444ba25b97b7775c1780f
Gruka469 <lifetroll6@gmail.com>
2023-11-10 21:17:52 +0200

    .

--- rage_quit.py
+++ rage_quit.py
@@ -1,16 +1,17 @@
 text = input()
-unique_symbols = ""
-repetitions = 0
+rage_message = ""
+repetitions = ""
 current_symbol = ""
-
 for index in range(len(text)):
-    if text[index].isdigit():
-        current_symbol += text[index]
-    else:
+    if not text[index].isdigit(): # We have non-numeric character
+        current_symbol += text[index].upper()
+    else: # We have digit here --> time for repetition
         for next_symbols in range(index, len(text)):
             if not text[next_symbols].isdigit():
                 break
             repetitions += text[next_symbols]
-        repetitions = int(repetitions)
-        unique_symbols += current_symbol * repetitions
-print(unique_symbols)
+        rage_message += current_symbol * int(repetitions)
+        current_symbol = ""
+        repetitions = ""
+print(f"Unique symbols used: {len(set(rage_message))}")
+print(rage_message)
--- winning_ticket.py
+++ winning_ticket.py
@@ -5,13 +5,17 @@ def check_ticket(ticket):
     left_part = ticket[:10]
     right_part = ticket[10:]
     for match_symbol in winning_symbols:
-        for uninterrupted_match_lenght in range(10, 5, -1):
-            winning_symbol_repetition = match_symbol * uninterrupted_match_lenght
-            if winning_symbol_repetition in left_part and winning_symbol_repetition in right_part:
-                if uninterrupted_match_lenght == 10:
-                    return f'ticket "{ticket}" - {uninterrupted_match_lenght}{match_symbol}'
-    return f"ticket '{ticket}' - no match"
-
+        for uninterrupted_match_length in range(10, 5, -1):
+            winning_symbol_repetition = match_symbol * uninterrupted_match_length
+            # We have winning ticket
+            if winning_symbol_repetition in left_part \
+                    and winning_symbol_repetition in right_part:
+                if uninterrupted_match_length == 10:
+                    return f'ticket "{ticket}" - {uninterrupted_match_length}{match_symbol} Jackpot!'
+                return f'ticket "{ticket}" - {uninterrupted_match_length}{match_symbol}'
+    return f'ticket "{ticket}" - no match'
+ 
+ 
 tickets = [ticket.strip() for ticket in input().split(", ")]
 for current_ticket in tickets:
     print(check_ticket(current_ticket))
